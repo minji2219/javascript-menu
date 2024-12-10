a7e595b9302dd5250c80993cb0fd5beca2947e53
minji2219 <200146@jnu.ac.kr>
2024-12-11 00:10:10 +0900

    feat: 숫자 형태의 카테고리 string으로 변환
    객체데이터 읽기, 출력 등을 위해 숫자 형태의 카테고리 스트링형태로 변환

--- src/Categories.js
+++ src/Categories.js
@@ -13,6 +13,30 @@ export class Categories {
       if (number > 1) continue;
       this.categories.push(random);
     }
-    return this.categories;
+    return this.makeCategoriesString();
+  }
+
+  makeCategoriesString() {
+    let categoriesString = [];
+    this.categories.map((category, idx) => {
+      switch (category) {
+        case 1:
+          this.categories[idx] = '일식';
+          break;
+        case 2:
+          this.categories[idx] = '한식';
+          break;
+        case 3:
+          this.categories[idx] = '중식';
+          break;
+        case 4:
+          this.categories[idx] = '아시안';
+          break;
+        case 5:
+          this.categories[idx] = '양식';
+          break;
+      }
+    });
+    return categoriesString;
   }
 }
