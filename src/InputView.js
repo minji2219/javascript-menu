a1e825d5eded9d7cff4786ba9bd62303953ef96c
minji2219 <200146@jnu.ac.kr>
2024-12-10 22:29:14 +0900

    feat: 코치별로 먹지 않는 음식 입력 받기
    ','로 구분하여 입력 받고, unEatMenu 배열에 이름, 음식들을 배열 데이터로 삽입

--- src/InputView.js
+++ src/InputView.js
@@ -12,5 +12,17 @@ const InputView = {
       return await this.readName();
     }
   },
+
+  async readUnEat(coaches) {
+    let unEatMenu = [];
+    for (const coach of coaches) {
+      const input = await Console.readLineAsync(`\n${coach}(이)가 못 먹는 메뉴를 입력해 주세요.`);
+      unEatMenu.push({
+        name: coach,
+        unEat: input.split(','),
+      });
+    }
+    return unEatMenu;
+  },
 };
 export default InputView;
